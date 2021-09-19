6389e9c9689ce6e39438825d72ca9471d53fd14a
jeremy black <jrebla85@yahoo.com>
2021-09-19 02:01:35 -0500

    reworked seeding for owners

--- data/seeds/01_owners.js
+++ data/seeds/01_owners.js
@@ -4,8 +4,32 @@ const bcrypt = require("bcryptjs");
 const PASSWORD1 = process.env.PASSWORD1
 const PASSWORD2 = process.env.PASSWORD2
 
-const hash1 = bcrypt.hashSync(PASSWORD1, 10);
-const hash2 = bcrypt.hashSync(PASSWORD2, 10);
+const encrypt1 = async () => {
+  try{
+    const hash1 = await bcrypt.hash(PASSWORD1, 10);
+
+    if (hash1){
+      return hash1
+    }
+  }catch(err){
+    console.log(err);
+  }
+}
+
+const encrypt2 = async () => {
+  try{
+    const hash2 = await bcrypt.hash(PASSWORD2, 10);
+
+    if (hash2){
+      return hash2
+    }
+  }catch(err){
+    console.log(err);
+  }
+}
+
+const pass1 = encrypt1();
+const pass2 = encrypt2();
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -17,13 +41,13 @@ exports.seed = function(knex) {
         {
           userName: 'JohnDoe',
           email: 'johndoe@email.com',
-          password: hash1,
+          password: pass1,
           location: 'Cape Town, South Africa'
         },
         {
           userName: 'JaneDoe',
           email: 'janedoe@email.com',
-          password: hash2,
+          password: pass2,
           location: 'Durban, South Africa'
         },
       ]);
